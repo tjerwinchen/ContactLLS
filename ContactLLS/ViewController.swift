bfc8eaa98e5b19b314b8fea0ec214948227fe338
Theo Chen <zhecui.chen@me.com>
2017-06-20 09:37:33 +0800

    Initial Commit

---
+++ ContactLLS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactLLS
+//
+//  Created by Theo Chen on 6/20/17.
+//  Copyright © 2017 Theo Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
